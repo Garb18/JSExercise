d187593ad0d33a577c5beb576b64cc482d02e42c
Garb18 <46030758+Garb18@users.noreply.github.com>
2020-08-03 20:31:43 +0100

    Initial Commit
    Enviornment set up and initial file structure

---
+++ src/JS/datetime.js
@@ -0,0 +1,9 @@
+let today = new Date();
+
+//Returns 0 - 6 based on current day
+let day = today.getDay();
+
+let dayList = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+
+console.log("Today is " + dayList[day] + ".")
+
